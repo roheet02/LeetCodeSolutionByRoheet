83669bcbc24b7f089c501a5f6158be8d40b16b4c
roheet02 <rohitfuke195@gmail.com>
2025-11-23 15:19:28 +0530

    Adding two sum input using Binary search tree

---
+++ codes/Graph/Tw0_sum_input_BST.py
@@ -0,0 +1,28 @@
+#BST tree
+class TreeNode:
+    def __init__(self,val=0,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+class Solution:
+    def findtarget(self,root,k):
+        ls=set()
+        def dfs(node):
+            if not node:
+                return False
+            if (k-node.val) in ls:
+                return True
+            ls.add(node.val)
+            return dfs(node.left) or dfs(node.right)
+        return dfs(root)
+
+root = TreeNode(5)
+root.left = TreeNode(3)
+root.right = TreeNode(6)
+root.left.left = TreeNode(2)
+root.left.right = TreeNode(4)
+root.right.right = TreeNode(7)
+
+
+result = Solution().findtarget(root,9)
+print(result)
