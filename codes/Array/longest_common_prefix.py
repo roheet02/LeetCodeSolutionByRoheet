fa16eb430068c2616591ce9fc7e4bcdaf923848b
roheet02 <rohitfuke195@gmail.com>
2025-11-28 09:28:59 +0530

    adding comments

--- codes/Array/longest_common_prefix.py
+++ codes/Array/longest_common_prefix.py
@@ -2,11 +2,11 @@ class Solution():
     def longestPrefix(self,strs):
         if not strs:
             return ""
-        prefix = strs[0]
+        prefix = strs[0] #will select the first element
 
         for i in strs[1:]:
-            while not i.startswith(prefix):
-                prefix=prefix[:-1]
+            while not i.startswith(prefix):  #i.startswith will check whether i and prefix is matching or not
+                prefix=prefix[:-1]  # this will start shrinking of the string
                 if not prefix:
                     return ""
         return prefix
