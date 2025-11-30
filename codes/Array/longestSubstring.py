26f7884599e596ae9d667e5f389c393faef09e07
roheet02 <rohitfuke195@gmail.com>
2025-11-30 14:32:50 +0530

    optimise solution

--- codes/Array/longestSubstring.py
+++ codes/Array/longestSubstring.py
@@ -1,4 +1,4 @@
-# longest substring usnig sliding window
+longest substring usnig sliding window
 
 class Solution:
     def longestSubstring(self, s:str):
@@ -14,4 +14,24 @@ class Solution:
         return max_len
 
 print(Solution().longestSubstring("ahidhshuc"))
-# print(Solution().longestSubstring("rohit"))
+print(Solution().longestSubstring("rohit"))
+
+
+#fastest
+class Solution:
+    def longestSubstring(self, s:str):
+        charSet=set()
+        left=0
+        max_len=0
+        for right in range(len(s)):
+            if s[right] not in charSet:
+                charSet.add(s[right])
+                max_len=max(max_len,right-left+1)
+            else:
+                while s[right] in charSet:
+                    charSet.remove(s[left])
+                    left+=1
+                charSet.add(s[right])
+        return max_len
+                
+print(Solution().longestSubstring("rohitrohitfuke"))
