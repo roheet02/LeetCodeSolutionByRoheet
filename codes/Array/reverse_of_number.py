def6e27c4ea8cec38aaac89898379c45b13e0179
roheet02 <rohitfuke195@gmail.com>
2025-12-03 08:06:46 +0530

    Adding more optimise solution for reverse number solution

--- codes/Array/reverse_of_number.py
+++ codes/Array/reverse_of_number.py
@@ -13,3 +13,19 @@ class Solution:
         return numm*sign
 
 print(Solution().reverseNumber(12349793))
+
+
+#solution2 more optimise
+
+class Solution:
+    def reverseNumber(self, x: int) -> int:
+        x1 = str(abs(x))
+        x2 = x1[::-1]
+        res = int(x2)
+        if x < 0:
+            res = -res
+        if res < -(2**31) or res > 2**31 - 1:
+            return 0
+        return res
+
+print(Solution().reverseNumber(12349793))
