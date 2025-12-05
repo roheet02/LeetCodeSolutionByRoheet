081dda6da6220de07ba42ee15b18972ea4f5c08b
roheet02 <rohitfuke195@gmail.com>
2025-12-05 10:11:47 +0530

    Adding the code to find and count the non duplicated numbers from list

---
+++ codes/Array/remove_duplicates_from_list.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self,nums):
+        if not nums:
+            return 0
+        k=1
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[i-1]:
+                nums[k]=nums[i]
+                k+=1
+        return k
+
+print(Solution().removeDuplicates([2,2,2,3,4,5,6]))
