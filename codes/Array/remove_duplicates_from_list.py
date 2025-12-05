c56a56a559f9fd290d3b01cc00912475098ee60d
roheet02 <rohitfuke195@gmail.com>
2025-12-05 10:13:01 +0530

    Adding comments

--- codes/Array/remove_duplicates_from_list.py
+++ codes/Array/remove_duplicates_from_list.py
@@ -2,9 +2,9 @@ class Solution:
     def removeDuplicates(self,nums):
         if not nums:
             return 0
-        k=1
+        k=1  #pointer to count unique numbers
         for i in range(1,len(nums)):
-            if nums[i]!=nums[i-1]:
+            if nums[i]!=nums[i-1]: #found a new unique number
                 nums[k]=nums[i]
                 k+=1
         return k
