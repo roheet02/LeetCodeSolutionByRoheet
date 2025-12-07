af4b9f789d060abd6b898fb6e62f98798273e1ca
roheet02 <rohitfuke195@gmail.com>
2025-12-07 16:57:49 +0530

    Adding comments

--- codes/mathematics_calculation/countODDnumberfromRange.py
+++ codes/mathematics_calculation/countODDnumberfromRange.py
@@ -1,3 +1,6 @@
+#  Count Odd Numbers in an Interval Range
+#  Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+#Solution1
 class Solution:
     def countOdds(self,low,high):
         count=0
@@ -9,7 +12,7 @@ class Solution:
 
 print(Solution().countOdds(6,8))
 
-
+#Solution2
 class Solution:
     def countOdds(self,low,high):
         return (high+1)//2-(low//2)
