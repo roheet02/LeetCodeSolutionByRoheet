8aa335bcde3b2705dabfd3b44e4af4811ff55924
roheet02 <rohitfuke195@gmail.com>
2025-12-15 20:51:05 +0530

    Adding another solution for gas station

--- codes/search/gas_station.py
+++ codes/search/gas_station.py
@@ -15,3 +15,22 @@ class Solution:
         return start if total_gas>=0 else -1
 
 print(Solution().canCompleteCircuit([1,2,3,4,5],[3,4,5,1,2]))
+
+#Solution2
+class Solution:
+    def canCompleteCircuit(self,gas,cost):
+        if sum(gas)<sum(cost):
+            return -1
+
+        n=len(gas)
+        running_gas_balance=0
+        lowest_gas=0
+        index_lowest_gas=float("inf")
+        for i in range(n):
+            running_gas_balance+=gas[i]-cost[i]
+            if running_gas_balance<lowest_gas:
+                lowest_gas=running_gas_balance
+                index_lowest_gas=i
+
+        return (index_lowest_gas+1)%n
+print(Solution().canCompleteCircuit([1,2,3,4,5],[3,4,5,1,2]))
