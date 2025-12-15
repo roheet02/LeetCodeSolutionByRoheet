5a084a499734c99ca4ab1822bfc98a6e7edb0622
roheet02 <rohitfuke195@gmail.com>
2025-12-15 19:49:29 +0530

    Adding product of array execpt self solution

---
+++ codes/search/product_of_array_except_self.py
@@ -0,0 +1,19 @@
+#Product of Array Except Self
+
+class Solution:
+    def productExceptSelf(self, nums):
+        n=len(nums)
+        answer=[1]*n
+        prefix=1
+        for i in range(n):
+            answer[i]=prefix
+            prefix*=nums[i]
+            print(prefix)
+        print(answer)
+        rightProduct=1
+        for i in range(n-1,-1,-1):
+            answer[i] *= rightProduct
+            rightProduct *= nums[i]
+            print(rightProduct)
+        return answer
+print(Solution().productExceptSelf([1,2,3,4]))
