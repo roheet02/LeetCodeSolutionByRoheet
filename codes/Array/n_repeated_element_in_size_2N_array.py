d385dfe78a6b2fd7890b9ca5eae784eec9ae3753
roheet02 <rohitfuke195@gmail.com>
2026-01-03 16:35:04 +0530

    comment

--- codes/Array/n_repeated_element_in_size_2N_array.py
+++ codes/Array/n_repeated_element_in_size_2N_array.py
@@ -5,10 +5,8 @@ from typing import List
 class Solution:
     def repeatedNTimes(self, nums):
         freq = {}
-
         for num in nums:
             if num in freq:
                 return num   # repeated element found
             freq[num] = 1
-
 print(Solution().repeatedNTimes([2,1,2,5,3,2]))
